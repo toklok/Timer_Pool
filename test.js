eb0e88c0451c1a34467e38df0bb52be6300ff2b4
toklok <joecurt85@gmail.com>
2015-07-01 17:44:18 -0400

    playing around with code

--- test.js
+++ test.js
@@ -1,3 +1,15 @@
-var gpio = require('rpi-gpio');
+var rpiGpio = require('rpi-gpio');
+var onoff = require('onoff').Gpio;
+
+
+var filter = new Gpio(29, 'out', {
+    setEdge
+});
+
+filter.watch(cb);
+
+var cb = function () {
+  console.log('ran');
+};
 
 console.log('works');
