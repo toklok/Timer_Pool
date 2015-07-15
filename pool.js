099fb81e6918fa9f47f8aef6eaff1ba107ec97b6
toklok <joecurt85@gmail.com>
2015-07-14 20:01:31 -0400

    in git

---
+++ pool.js
@@ -0,0 +1,35 @@
+var Gpio = require('onoff').Gpio,
+    filter = new Gpio(24, 'out', 'both'),
+    relay = new Gpio(4, 'out', 'none');
+
+
+//Watches filter for changes and acts on those changes
+
+filter.watch(function (err, value) {
+
+    if (err) {
+        throw err;
+    }
+
+    if (value <= 1) {
+
+        //On
+
+        relay.setDirection('in');
+
+    }
+
+    if (value >=1) {
+
+        //Off
+
+        relay.setDirection('out');
+
+        console.log('off');
+
+    }
+
+
+});
+
+
